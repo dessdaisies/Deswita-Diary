9e3cceebea16f5bac3f0f1f28ca8045b10ee1059
dessdaisies <dsalsabila725@gmail.com>
2026-05-21 05:16:54 +0800

    session

--- simpan_favorite.php
+++ simpan_favorite.php
@@ -1,17 +1,14 @@
 <?php
 include "koneksi.php";
 
-$kat  = $_POST['kategori'];
-$fav  = $_POST['nama_favorite']; 
+$kat = $_POST['kategori'];
+$fav = $_POST['nama_favorit'];
 
-$query = "INSERT INTO favorite (kategori, favorite) VALUES ('$kat', '$fav')";
-$hasil = mysqli_query($koneksi, $query);
+$query = "INSERT INTO favorit (kategori, favorit) VALUES ('$kat', '$fav')";
 
-if ($hasil) {
-    echo "<script>alert('Berhasil tambah favorite baru!'); window.location='tampil_favorite.php';</script>";
-} else {
-    echo "Gagal simpan: " . mysqli_error($koneksi);
+if(mysqli_query($koneksi, $query)){
+    echo "<script>alert('Berhasil nambah favorit!'); window.location='tampil_favorite.php';</script>";
+}else{
+    echo "Error: " . mysqli_error($koneksi);
 }
-
-mysqli_close($koneksi); 
 ?>
